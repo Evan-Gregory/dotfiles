220b2a7bd7caed9aff1e7a4e6de0adc21b076b99
Evan Gregory <evan.gregory@streametric.io>
2025-02-24 10:26:17 -0500

    ✨feat: misc plugins (need to add files for each)

--- nvim/lua/custom/plugins/vim-surround.lua
+++ nvim/lua/custom/plugins/vim-surround.lua
@@ -1,13 +1,66 @@
 return {
+  --  {
+  --    'tpope/vim-surround',
+  --  },
+  --  {
+  --    'kana/vim-textobj-user',
+  --  },
   {
-    'tpope/vim-surround',
+    --    'lukas-reineke/indent-blankline.nvim',
+    --    main = 'ibl',
+    --    event = 'BufReadPost',
+    --    ---@module "ibl"
+    --    ---@type ibl.config
+    --    opts = {
+    --      scope = {
+    --        show_start = false,
+    --        show_end = false,
+    --      },
+    --      config = function(_, opts) end,
+    --    },
   },
+
   {
-    'kana/vim-textobj-user',
+    'shellRaining/hlchunk.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local cb = function()
+        if vim.g.colors_name == 'tokyonight' then
+          return '#806d9c'
+        else
+          return '#00ffff'
+        end
+      end
+      require('hlchunk').setup {
+        chunk = { enable = true },
+        indent = { enable = true },
+      }
+    end,
   },
-  { 'lukas-reineke/indent-blankline.nvim' },
-  { 'stevearc/dressing.nvim' },
-  --{
-  --  'Julian/vim-textobj-variable-segment',
-  --},
+  --  { 'stevearc/dressing.nvim' },
+  -- lazy.nvim
+  {
+    'folke/noice.nvim',
+    priority = 500,
+    event = 'VeryLazy',
+    opts = {
+      lsp = {
+        hover = {
+          enabled = false,
+        },
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+  --  --{
+  --  --  'Julian/vim-textobj-variable-segment',
+  --  --},
 }
