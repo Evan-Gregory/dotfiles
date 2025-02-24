75534a0ded1a2ded396a214a24c7886c786ea76e
Evan Gregory <evan.gregory@streametric.io>
2025-02-24 13:41:08 -0500

    ✨feat: add crates.nvim (remove?)

---
+++ nvim/lua/custom/plugins/crates.lua
@@ -0,0 +1,25 @@
+-- Managing crate dependencies.
+return {
+  {
+    'Saecki/crates.nvim',
+    event = 'BufRead Cargo.toml',
+    opts = {
+      popup = { border = 'rounded' },
+    },
+    config = function(_, opts)
+      local crates = require 'crates'
+
+      crates.setup(opts)
+
+      local function keymap(key, rhs, desc)
+        vim.keymap.set('n', '<leader>cc' .. key, function()
+          rhs()
+          crates.focus_popup()
+        end, { desc = desc })
+      end
+      keymap('c', crates.show_crate_popup, 'Show crate popup')
+      keymap('f', crates.show_features_popup, 'Show features popup')
+      keymap('v', crates.show_versions_popup, 'Show versions popup')
+    end,
+  },
+}
