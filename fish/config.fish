e1689e0e3e00fa7ea954dca5230457ee40cba24e
Evan Gregory <evan.gregory@streametric.io>
2025-11-05 09:47:02 -0500

    ✨ feat: add completion for jiratui

--- fish/config.fish
+++ fish/config.fish
@@ -134,6 +134,23 @@ function wa
     curl -s $url
 end
 
+function _jiratui_completion;
+    set -l response (env _JIRATUI_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) jiratui);
+
+    for completion in $response;
+        set -l metadata (string split "," $completion);
+
+        if test $metadata[1] = "dir";
+            __fish_complete_directories $metadata[2];
+        else if test $metadata[1] = "file";
+            __fish_complete_path $metadata[2];
+        else if test $metadata[1] = "plain";
+            echo $metadata[2];
+        end;
+    end;
+end;
+
+complete --no-files --command jiratui --arguments "(_jiratui_completion)";
 # Get terminal emulator
 # set TERM_EMULATOR (ps -aux | grep (ps -p $fish_pid -o ppid=) | awk 'NR==1{print $11}')
 
